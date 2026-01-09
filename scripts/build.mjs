3815fe73e1de83656b2c72a0d074c147bf060929
Ricardo Belfort <rbelfort2004@gmail.com>
2026-01-08 21:32:35 -0400

    ajuste

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -94,9 +94,8 @@ function generateHTML(content, frontmatter, route) {
   <meta property="og:url" content="${canonical}" />
   <link rel="canonical" href="${canonical}" />
   <title>${escapeHtml(title)}</title>
-  <link rel="preload" href="/assets/css/base.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
-  <link rel="preload" href="/assets/css/components.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
-  <noscript><link rel="stylesheet" href="/assets/css/base.css" /><link rel="stylesheet" href="/assets/css/components.css" /></noscript>
+  <link rel="stylesheet" href="/assets/css/base.css" />
+  <link rel="stylesheet" href="/assets/css/components.css" />
   <script type="application/ld+json">
   {
     "@context": "https://schema.org",
